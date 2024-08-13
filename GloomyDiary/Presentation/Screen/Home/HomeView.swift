d241fe16354c5156f5e3a196287b46aafbe21e21
LURKS02 <thecrooks2@gmail.com>
2024-08-14 01:12:15 +0900

    Home 뷰 구성하기

--- GloomyDiary/Presentation/Screen/Home/HomeView.swift
+++ GloomyDiary/Presentation/Screen/Home/HomeView.swift
@@ -8,9 +8,75 @@
 import UIKit
 
 final class HomeView: BaseView {
+    struct Matric {
+        static let moonTopPadding: CGFloat = 132
+        static let ghostMoonPadding: CGFloat = 122
+        static let ghostLeftPadding: CGFloat = 61
+        static let ghostButtonPadding: CGFloat = 65
+        
+        static let stackViewSpacing: CGFloat = 14
+        static let ghostImageRightPadding: CGFloat = 25
+        
+        static let moonImageSize: CGFloat = 43
+        static let ghostImageSize: CGFloat = 78
+    }
+    
+    let gradientView: GradientView = GradientView(colors: [.background(.darkPurple),
+                                                           .background(.mainPurple),
+                                                           .background(.mainPurple)])
+    
+    let moonImageView = ImageView(imageName: "moon",
+                                  size: Matric.moonImageSize)
+    
+    let ghostImageView = ImageView(imageName: "ghost",
+                                   size: Matric.ghostImageSize)
+    
+    let talkingView: TalkingView = TalkingView(text: "안녕!\n오늘은 어떤 고민이 있니?").then {
+        $0.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+    }
+    
+    lazy var ghostStackView = UIStackView(arrangedSubviews: [talkingView, ghostImageView]).then {
+        $0.axis = .vertical
+        $0.spacing = Matric.stackViewSpacing
+        $0.alignment = .trailing
+        $0.distribution = .fill
+    }
+    
+    let startButton = ButtonView(text: "상담하기")
+    
     override func setup() {
         super.setup()
+    }
+    
+    override func addSubviews() {
+        addSubview(gradientView)
+        addSubview(moonImageView)
+        addSubview(startButton)
+        addSubview(ghostStackView)
+    }
+    
+    override func setupConstraints() {
+        gradientView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
         
-        self.backgroundColor = .red
+        moonImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(Matric.moonTopPadding)
+        }
+        
+        ghostStackView.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(Matric.ghostLeftPadding)
+            make.top.equalTo(moonImageView.snp.bottom).offset(Matric.ghostMoonPadding)
+        }
+        
+        ghostImageView.snp.makeConstraints { make in
+            make.right.equalToSuperview().offset(Matric.ghostImageRightPadding)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(ghostStackView.snp.bottom).offset(Matric.ghostButtonPadding)
+        }
     }
 }
