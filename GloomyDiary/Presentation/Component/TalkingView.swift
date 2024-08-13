dad1cc0a6d08a18111622e9d79525e86ecd0fd04
LURKS02 <thecrooks2@gmail.com>
2024-08-14 01:10:42 +0900

    말풍선 뷰 만들기

---
+++ GloomyDiary/Presentation/Component/TalkingView.swift
@@ -0,0 +1,49 @@
+//
+//  TalkingView.swift
+//  GloomyDiary
+//
+//  Created by 디해 on 8/13/24.
+//
+
+import UIKit
+
+final class TalkingView: BaseView {
+    private struct Matric {
+        static let verticalPadding: CGFloat = 18
+        static let horizontalPadding: CGFloat = 23
+        static let cornerRadius: CGFloat = 20
+    }
+    
+    private let talkingLabel: UILabel = UILabel().then {
+        $0.textColor = .text(.highlight)
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
+        $0.font = .무궁화.title
+    }
+    
+    init(text: String) {
+        super.init(frame: .zero)
+        self.talkingLabel.text = text
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setup() {
+        self.backgroundColor = .component(.darkPurple)
+        self.layer.cornerRadius = Matric.cornerRadius
+        self.layer.masksToBounds = true
+    }
+    
+    override func addSubviews() {
+        self.addSubview(talkingLabel)
+    }
+    
+    override func setupConstraints() {
+        talkingLabel.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview().inset(Matric.verticalPadding)
+            make.horizontalEdges.equalToSuperview().inset(Matric.horizontalPadding)
+        }
+    }
+}
