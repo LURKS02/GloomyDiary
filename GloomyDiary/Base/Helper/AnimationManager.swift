9b19dbc0e6d0e03688d4e39e5c542b2f0f7d4b1a
LURKS02 <thecrooks2@gmail.com>
2024-08-24 20:12:44 +0900

    Animation 및 매니저 수정하기

--- GloomyDiary/Base/Helper/AnimationManager.swift
+++ GloomyDiary/Base/Helper/AnimationManager.swift
@@ -12,33 +12,45 @@ final class AnimationManager {
     
     private init() { }
     
-    func moveUpAndDown(_ view: UIView, value: Double, withDuration duration: TimeInterval = 1.0) {
-        moveUpCycle(view, value: value, withDuration: duration)
-    }
-}
-
-extension AnimationManager {
-    private func moveUpCycle(_ view: UIView, value: Double, withDuration duration: TimeInterval) {
-        let animator = UIViewPropertyAnimator(duration: duration, curve: .easeInOut) { [weak view] in
-            view?.transform = .identity.translatedBy(x: 0, y: -value)
+    func run(animations: [Animation], mode: Mode) {
+        switch mode {
+        case .infinite:
+            runLoopAnimations(animations: animations)
+        case .once:
+            runAnimations(animations: animations)
         }
-        animator.addCompletion { [weak self] position in
-            if position == .end {
-                self?.moveDownCycle(view, value: value, withDuration: duration)
-            }
+    }
+    
+    private func runLoopAnimations(animations: [Animation], index: Int = 0) {
+        let animation = animations[index]
+        let animator = UIViewPropertyAnimator(duration: animation.duration,
+                                              curve: animation.curve,
+                                              animations: animation.closure)
+        animator.addCompletion { [weak self] _ in
+            let nextIndex = (index + 1) % animations.count
+            self?.runLoopAnimations(animations: animations, index: nextIndex)
         }
         animator.startAnimation()
     }
     
-    private func moveDownCycle(_ view: UIView, value: Double, withDuration duration: TimeInterval) {
-        let animator = UIViewPropertyAnimator(duration: duration, curve: .easeInOut) { [weak view] in
-            view?.transform = .identity.translatedBy(x: 0, y: value)
-        }
-        animator.addCompletion { [weak self] position in
-            if position == .end {
-                self?.moveUpCycle(view, value: value, withDuration: duration)
+    private func runAnimations(animations: [Animation], index: Int = 0) {
+        let animation = animations[index]
+        let animator = UIViewPropertyAnimator(duration: animation.duration,
+                                              curve: animation.curve,
+                                              animations: animation.closure)
+        animator.addCompletion { [weak self] _ in
+            let nextIndex = (index + 1)
+            if nextIndex < animations.count {
+                self?.runAnimations(animations: animations, index: nextIndex)
             }
         }
         animator.startAnimation()
+    }
+}
+
+extension AnimationManager {
+    enum Mode {
+        case infinite
+        case once
     }
 }
