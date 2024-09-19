bc5e0e78c06654f2833868aebedd8c9739835c56
LURKS02 <thecrooks2@gmail.com>
2024-09-19 15:44:05 +0900

    캐릭터 메시지 추가 (첫 인사, 로딩 메시지)

--- GloomyDiary/Model/Character.swift
+++ GloomyDiary/Model/Character.swift
@@ -64,6 +64,40 @@ enum Character: CaseIterable {
             "나에게 상담해봐~"
         }
     }
+    
+    var greetingMessage: String {
+        switch self {
+        case .chan:
+            "안녕! 나는 찬이야.\n" +
+            "오늘은 어떤 고민이 있어서 찾아온거니?\n" +
+            "\n" +
+            "고민, 걱정, 생각 등\n" +
+            "나에게 하고 싶은 말을 편지로 보내줘!"
+            
+        case .gomi:
+            "안녕? 난 고미야.\n" +
+            "오늘은 어떤 일로 찾아온거야?\n" +
+            "\n" +
+            "무슨 일이 있었는지\n" +
+            "나에게 편지를 보내줘."
+            
+        case .beomji:
+            "안녕~ 나는 범지야.\n" +
+            "불안하거나 초조한 일이 있었니?\n" +
+            "\n" +
+            "걱정 말고 편한 마음으로\n" +
+            "편지를 보내주라~"
+        }
+    }
+    
+    var counselReadyMessage: String {
+        switch self {
+        case .chan:
+            "보물 창고를 뒤적이는 중 ..."
+        case .gomi:
+            "꽁꽁 얽힌 실타래를 푸는 중 ..."
+        case .beomji:
+            "읽던 책을 정리하는 중 ..."
         }
     }
 }
