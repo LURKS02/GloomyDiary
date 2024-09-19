636a496d493dadaa560eb24674b010cc97618052
LURKS02 <thecrooks2@gmail.com>
2024-09-19 15:52:03 +0900

    Choosing에서 캐릭터 String 메시지를 제공하지 않도록 수정

--- GloomyDiary/Presentation/Screen/Home/Choosing/Choosing.swift
+++ GloomyDiary/Presentation/Screen/Home/Choosing/Choosing.swift
@@ -12,35 +12,28 @@ import ComposableArchitecture
 struct Choosing {
     @ObservableState
     struct State: Equatable {
-        var firstIntroduceText: String = ""
-        var secondIntroduceText: String = ""
-        var characterIntroduceText: String = ""
+        var isFirst: Bool = false
         var chosenCharacter: Character? = nil
     }
     
     enum Action {
-        case initialize(Bool)
-        case characterTapped(character: Character)
+        case characterTapped(tag: Int)
     }
     
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
-            case .initialize(let isFirstConsultation):
-                if isFirstConsultation {
-                    state.firstIntroduceText = "첫 번째 상담을 진행해볼까요?"
-                    state.secondIntroduceText = "\"울다\"에는\n여러분들의 이야기를 들려줄\n세 마리의 상담사가 있어요."
-                } else {
-                    state.firstIntroduceText = "반가워요!"
-                    state.secondIntroduceText = "오늘은 어떤 일이 있었나요?\n당신의 이야기를 들려주세요."
+            case .characterTapped(let tag):
+                switch tag {
+                case 0:
+                    state.chosenCharacter = .chan
+                case 1:
+                    state.chosenCharacter = .gomi
+                case 2:
+                    state.chosenCharacter = .beomji
+                default:
+                    state.chosenCharacter = nil
                 }
-                
-                state.characterIntroduceText = "상담하고 싶은 친구를\n선택해주세요."
-                return .none
-                
-            case .characterTapped(let character):
-                state.chosenCharacter = character
-                state.characterIntroduceText = character.introduce
                 return .none
             }
         }
