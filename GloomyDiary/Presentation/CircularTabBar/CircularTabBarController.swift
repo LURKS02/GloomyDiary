98aae6dcb871897f4c26c47d2c2f5356e39f6ce0
LURKS02 <thecrooks2@gmail.com>
2024-10-27 19:07:53 +0900

    circular tab bar controller 구현하기

---
+++ GloomyDiary/Presentation/CircularTabBar/CircularTabBarController.swift
@@ -0,0 +1,121 @@
+//
+//  CircularTabBarController.swift
+//  GloomyDiary
+//
+//  Created by 디해 on 10/24/24.
+//
+
+import UIKit
+
+final class CircularTabBarController: UITabBarController {
+    
+    private let circularTabBar: CircularTabBar
+    
+    private var currentIndex: Int {
+        didSet {
+            selectedIndex = currentIndex
+        }
+    }
+    
+    init(tabBarItems: [CircularTabBarItem], currentIndex: Int = 0) {
+        self.currentIndex = currentIndex
+        self.circularTabBar = CircularTabBar(items: tabBarItems)
+        
+        super.init(nibName: nil, bundle: nil)
+        
+        self.viewControllers = tabBarItems.map { $0.viewController }
+        self.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+        addSubviews()
+        setupTabBar()
+        setupTapGesture()
+    }
+    
+    private func setup() {
+        self.tabBar.isHidden = true
+    }
+    
+    private func addSubviews() {
+        view.addSubview(circularTabBar)
+    }
+    
+    private func setupTabBar() {
+        view.backgroundColor = .white
+        
+        circularTabBar.frame = CGRect(x: view.bounds.width / 2 - circularTabBar.diameter / 2,
+                                      y: view.bounds.height - circularTabBar.diameter / 2 + 10,
+                                      width: circularTabBar.diameter,
+                                      height: circularTabBar.diameter)
+        
+        highlightCurrentTab()
+    }
+    
+    private func setupTapGesture() {
+        circularTabBar.tabBarTappedRelay
+            .subscribe(onNext: { [weak self] _ in
+                guard let self else { return }
+                self.tabTapped()
+            })
+            .disposed(by: rx.disposeBag)
+    }
+    
+    private func tabTapped() {
+        currentIndex = (currentIndex + 1) % circularTabBar.numberOfTabs
+        rotateTabBar(index: currentIndex)
+        highlightCurrentTab()
+    }
+    
+    private func rotateTabBar(index: Int) {
+        let angle: CGFloat = (2 * .pi / CGFloat(circularTabBar.numberOfTabs)) * CGFloat(index)
+        UIView.animate(withDuration: 0.6, delay: 0, options: .curveEaseInOut, animations: {
+            self.circularTabBar.contentView.transform = .identity.rotated(by: -angle)
+        })
+    }
+    
+    private func highlightCurrentTab() {
+        for (index, icon) in circularTabBar.icons.enumerated() {
+            UIView.transition(with: icon, duration: 0.3, options: .transitionCrossDissolve, animations: {
+                self.circularTabBar.highlightIcon(index: index, isHighlighted: (index == self.currentIndex))
+            }, completion: nil)
+        }
+    }
+}
+
+extension CircularTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> (any UIViewControllerAnimatedTransitioning)? {
+        TabSwitchingTransition()
+    }
+}
+
+extension CircularTabBarController: CircularTabBarControllable {
+    func hideCircularTabBar(duration: TimeInterval) async {
+        await withCheckedContinuation { continuation in
+            AnimationGroup(animations: [.init(view: circularTabBar,
+                                              animationCase: .fadeOut,
+                                              duration: duration)],
+                           mode: .parallel,
+                           loop: .once(completion: { continuation.resume() }))
+            .run()
+        }
+    }
+
+    func showCircularTabBar(duration: TimeInterval) async {
+        await withCheckedContinuation { continuation in
+            AnimationGroup(animations: [.init(view: circularTabBar,
+                                              animationCase: .fadeIn,
+                                              duration: duration)],
+                           mode: .parallel,
+                           loop: .once(completion: { continuation.resume() }))
+            .run()
+        }
+    }
+}
---
+++ GloomyDiary/Presentation/CircularTabBar/CircularTabBarItem.swift
@@ -0,0 +1,55 @@
+//
+//  CircularTabBarItem.swift
+//  GloomyDiary
+//
+//  Created by 디해 on 10/25/24.
+//
+
+import UIKit
+import ComposableArchitecture
+
+enum CircularTabBarItem: String {
+    case home
+    case history
+    case diary
+}
+
+extension CircularTabBarItem {
+    var viewController: UIViewController {
+        switch self {
+        case .home:
+            let store = Store(initialState: Home.State()) {
+                Home()
+            }
+            return HomeViewController(store: store)
+            
+        case .history:
+            let store = Store(initialState: History.State()) { History() }
+            return NavigationController(rootViewController: HistoryViewController(store: store))
+            
+        case .diary:
+            return DiaryViewController()
+        }
+    }
+}
+
+extension CircularTabBarItem {
+    var title: String {
+        switch self {
+        case .home:
+            "홈"
+        case .history:
+            "히스토리"
+        case .diary:
+            "다이어리"
+        }
+    }
+    
+    var normalImage: UIImage {
+        UIImage(named: self.rawValue + "_unselected")!
+    }
+    
+    var selectedImage: UIImage {
+        UIImage(named: self.rawValue + "_selected")!
+    }
+}
