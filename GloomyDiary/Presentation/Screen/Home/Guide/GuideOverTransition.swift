198369120de637d28aa30b3c4dca48d9b55ed151
LURKS02 <thecrooks2@gmail.com>
2024-11-23 02:13:54 +0900

    처음 진입하는 유저의 가이드 화면 (GuideViewController) 구현

---
+++ GloomyDiary/Presentation/Screen/Home/Guide/GuideOverTransition.swift
@@ -0,0 +1,46 @@
+//
+//  GuideOverTransition.swift
+//  GloomyDiary
+//
+//  Created by 디해 on 11/22/24.
+//
+
+import UIKit
+
+final class GuideOverTransition: NSObject { }
+
+extension GuideOverTransition: UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: (any UIViewControllerContextTransitioning)?) -> TimeInterval {
+        2.0
+    }
+    
+    func animateTransition(using transitionContext: any UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        guard let fromView = transitionContext.view(forKey: .from) as? GuideView,
+              let toView = transitionContext.view(forKey: .to) as? StartCounselingView,
+              let fromViewController = transitionContext.viewController(forKey: .from) as? GuideViewController else { return }
+        
+        toView.alpha = 0.0
+        containerView.addSubview(toView)
+        
+        Task { @MainActor in
+            await fromView.hideAllComponents()
+            toView.hideAllComponents()
+            toView.moonImageView.alpha = 0.0
+            toView.alpha = 1.0
+            
+            await withCheckedContinuation { continuation in
+                AnimationGroup(animations: [.init(view: toView.moonImageView,
+                                                  animationCase: .fadeIn,
+                                                  duration: 1.0)],
+                               mode: .parallel,
+                               loop: .once(completion: { continuation.resume() }))
+                .run()
+            }
+            await toView.playFadeInFirstPart()
+            await toView.playFadeInSecondPart()
+            
+            transitionContext.completeTransition(true)
+        }
+    }
+}
