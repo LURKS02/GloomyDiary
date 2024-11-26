dea99be28c13e1648b9c1e7a6063d0d80acabe86
LURKS02 <thecrooks2@gmail.com>
2024-11-26 23:42:34 +0900

    BaseViewController의 viewWillAppear에서 로그를 남기도록 수정

--- GloomyDiary/Base/BaseViewController.swift
+++ GloomyDiary/Base/BaseViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 import RxSwift
 
-class BaseViewController<T: UIView>: UIViewController {
+class BaseViewController<T: BaseView>: UIViewController {
+    private let logID: String
+    
     var contentView: T
     
-    init(_ contentView: T = T()) {
+    init(_ contentView: T = T(), logID: String) {
         self.contentView = contentView
+        self.logID = logID
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -22,5 +25,11 @@ class BaseViewController<T: UIView>: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        Logger.send(type: .screen, logID)
     }
 }
