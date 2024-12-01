41eccbc61b677bc4ff2abe849dc0138c57c8162c
LURKS02 <thecrooks2@gmail.com>
2024-12-02 04:45:56 +0900

    유효한 결과 뷰인 ValidResultView 구현

---
+++ GloomyDiary/Presentation/Screen/Home/Result/ValidResultView.swift
@@ -0,0 +1,119 @@
+//
+//  ValidResultView.swift
+//  GloomyDiary
+//
+//  Created by 디해 on 12/2/24.
+//
+
+import UIKit
+
+final class ValidResultView: BaseView {
+    
+    // MARK: - Views
+    
+    let characterImageView: ImageView = ImageView().then {
+        $0.setSize(87)
+    }
+    
+    let resultLetterView = ResultLetterView()
+    
+    let shareButton: HorizontalButton = HorizontalButton().then {
+        $0.setTitle("공유하기", for: .normal)
+        $0.backgroundColor = .component(.blackPurple)
+    }
+    
+    let homeButton: HorizontalButton = HorizontalButton().then {
+        $0.setTitle("홈으로", for: .normal)
+    }
+    
+    
+    // MARK: - View Life Cycle
+    
+    override func setup() {
+        backgroundColor = .background(.mainPurple)
+    }
+    
+    override func addSubviews() {
+        addSubview(characterImageView)
+        addSubview(resultLetterView)
+        addSubview(shareButton)
+        addSubview(homeButton)
+    }
+    
+    override func setupConstraints() {
+        characterImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(71)
+            make.centerX.equalToSuperview()
+        }
+        
+        resultLetterView.snp.makeConstraints { make in
+            make.top.equalTo(characterImageView.snp.bottom).offset(14)
+            make.leading.equalToSuperview().offset(17)
+            make.trailing.equalToSuperview().offset(-17)
+            make.bottom.equalToSuperview().offset(-218)
+        }
+        
+        shareButton.snp.makeConstraints { make in
+            make.top.equalTo(resultLetterView.snp.bottom).offset(25)
+            make.centerX.equalToSuperview()
+        }
+        
+        homeButton.snp.makeConstraints { make in
+            make.top.equalTo(shareButton.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    func configure(with character: CharacterDTO) {
+        characterImageView.setImage(character.imageName)
+    }
+}
+
+// MARK: - Animations
+
+extension ValidResultView {
+    func hideAllComponents() {
+        subviews.filter { $0 != characterImageView }.forEach { $0.alpha = 0.0 }
+    }
+    
+    @MainActor
+    func playAllComponentsFadeIn() async {
+        hideAllComponents()
+        
+        await withCheckedContinuation { continuation in
+            AnimationGroup(animations: [.init(view: resultLetterView,
+                                              animationCase: .fadeIn,
+                                              duration: 1.0),
+                                        .init(view: homeButton,
+                                              animationCase: .fadeIn,
+                                              duration: 1.0),
+                                        .init(view: shareButton,
+                                              animationCase: .fadeIn,
+                                              duration: 1.0)],
+                           mode: .parallel,
+                           loop: .once(completion: { continuation.resume() }))
+            .run()
+        }
+    }
+    
+    @MainActor
+    func playAllComponentsFadeOut() async {
+        await withCheckedContinuation { continuation in
+            AnimationGroup(animations: [.init(view: resultLetterView,
+                                              animationCase: .fadeOut,
+                                              duration: 1.0),
+                                        .init(view: homeButton,
+                                              animationCase: .fadeOut,
+                                              duration: 1.0),
+                                        .init(view: characterImageView,
+                                              animationCase: .fadeOut,
+                                              duration: 1.0),
+                                        .init(view: shareButton,
+                                              animationCase: .fadeOut,
+                                              duration: 1.0)],
+                           mode: .parallel,
+                           loop: .once(completion: { continuation.resume() }))
+            .run()
+        }
+    }
+}
