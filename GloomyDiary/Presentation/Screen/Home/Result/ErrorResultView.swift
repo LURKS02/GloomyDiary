5c48df82917fa816f064bcfb1b2c1924d9a81430
LURKS02 <thecrooks2@gmail.com>
2024-12-02 04:45:44 +0900

    에러 표시 화면 ErrorResultView 구현

---
+++ GloomyDiary/Presentation/Screen/Home/Result/ErrorResultView.swift
@@ -0,0 +1,114 @@
+//
+//  ErrorResultView.swift
+//  GloomyDiary
+//
+//  Created by 디해 on 12/2/24.
+//
+
+import UIKit
+
+final class ErrorResultView: BaseView {
+    
+    // MARK: - Views
+    
+    let characterImageView = ImageView().then {
+        $0.setSize(87)
+    }
+    
+    let introduceLabel = IntroduceLabel().then {
+        $0.text = "일시적인 오류가 발생했어요."
+    }
+    
+    let subIntroduceLabel = IntroduceLabel().then {
+        $0.text = "네트워크가 잘 연결되어 있는지 확인해보세요.\n" +
+                  "잠시 기다렸다가 다시 시도해보세요.\n"
+        $0.textColor = .text(.subHighlight)
+    }
+    
+    let backButton = HorizontalButton().then {
+        $0.setTitle("뒤로가기", for: .normal)
+        $0.backgroundColor = .component(.blackPurple)
+    }
+    
+    let homeButton = HorizontalButton().then {
+        $0.setTitle("홈으로", for: .normal)
+    }
+    
+    
+    // MARK: - View Life Cycle
+    
+    override func setup() {
+        backgroundColor = .background(.mainPurple)
+    }
+    
+    override func addSubviews() {
+        addSubview(characterImageView)
+        addSubview(introduceLabel)
+        addSubview(subIntroduceLabel)
+        addSubview(backButton)
+        addSubview(homeButton)
+    }
+
+    override func setupConstraints() {
+        characterImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(100)
+            make.centerX.equalToSuperview()
+        }
+        
+        introduceLabel.snp.makeConstraints { make in
+            make.top.equalTo(characterImageView.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        subIntroduceLabel.snp.makeConstraints { make in
+            make.top.equalTo(introduceLabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        backButton.snp.makeConstraints { make in
+            make.top.equalTo(subIntroduceLabel.snp.bottom).offset(30)
+            make.centerX.equalToSuperview()
+        }
+        
+        homeButton.snp.makeConstraints { make in
+            make.top.equalTo(backButton.snp.bottom).offset(10)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    func configure(with character: CharacterDTO) {
+        characterImageView.setImage(character.cryingImageName)
+    }
+}
+
+extension ErrorResultView {
+    func hideAllComponents() {
+        subviews.filter { $0 != characterImageView }.forEach { $0.alpha = 0.0 }
+    }
+    
+    @MainActor
+    func playAllComponentsFadeIn() async {
+        hideAllComponents()
+        
+        await withCheckedContinuation { continuation in
+            AnimationGroup(animations: subviews.filter { $0 != characterImageView }.map { Animation(view: $0,
+                                                                                                    animationCase: .fadeIn,
+                                                                                                    duration: 1.0) },
+                           mode: .parallel,
+                           loop: .once(completion: { continuation.resume() }))
+            .run()
+        }
+    }
+    
+    @MainActor
+    func playAllComponentsFadeOut() async {
+        await withCheckedContinuation { continuation in
+            AnimationGroup(animations: subviews.map { Animation(view: $0,
+                                                                animationCase: .fadeOut,
+                                                                duration: 1.0) },
+                           mode: .parallel,
+                           loop: .once(completion: { continuation.resume() }))
+            .run()
+        }
+    }
+}
