c1c11dc8f1dd5b5fa4b6cc8e121f6a159170ab76
LURKS02 <thecrooks2@gmail.com>
2024-12-11 00:25:03 +0900

    새로운 대사 추가

--- GloomyDiary/Model/Talking.swift
+++ GloomyDiary/Model/Talking.swift
@@ -11,6 +11,8 @@ enum Talking: CaseIterable {
     case hello
     case cloud
     case walking
+    case sky
+    case pudding
     
     var description: String {
         switch self {
@@ -20,6 +22,10 @@ enum Talking: CaseIterable {
             "하늘에 있는 구름은\n가끔 보면\n몽실몽실한 소금빵 같아!"
         case .walking:
             "가끔은 마음 편히\n산책이라도 갈까?"
+        case .sky:
+            "밤하늘에\n반짝이는 별이 가득해!"
+        case .pudding:
+            "달콤한 커스터드 푸딩이\n먹고 싶은데..."
         }
     }
 }
