438cf6736ba1829d54109a67c9d7e010601391e8
LURKS02 <thecrooks2@gmail.com>
2025-01-12 19:17:01 +0900

    셀 내부 컬렉션 뷰가 diffabledatasource를 사용하도록 수정

---
+++ GloomyDiary/Presentation/Screen/History/HistoryList/CollectionView/HistoryImageItem.swift
@@ -0,0 +1,12 @@
+//
+//  HistoryImageItem.swift
+//  GloomyDiary
+//
+//  Created by 디해 on 1/12/25.
+//
+
+import Foundation
+
+struct HistoryImageItem: Hashable {
+    let url: URL
+}
---
+++ GloomyDiary/Presentation/Screen/History/HistoryList/CollectionView/HistoryImageSection.swift
@@ -0,0 +1,12 @@
+//
+//  HistoryImageSection.swift
+//  GloomyDiary
+//
+//  Created by 디해 on 1/12/25.
+//
+
+import Foundation
+
+enum HistoryImageSection {
+    case main
+}
