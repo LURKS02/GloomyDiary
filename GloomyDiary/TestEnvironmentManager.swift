8f727816c234f0737b2395ab8105babef28bd32b
LURKS02 <thecrooks2@gmail.com>
2025-01-17 18:55:21 +0900

    이미지 캐시에 따라 디버그 환경 설정 코드 수정

--- GloomyDiary/TestEnvironmentManager.swift
+++ GloomyDiary/TestEnvironmentManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Dependencies
+import UIKit.UIImage
 
 final class TestEnvironmentManager {
     @Dependency(\.counselingSessionRepository) var counselingSessionRepository
@@ -29,9 +30,17 @@ extension TestEnvironmentManager {
                 return URL(string: urlString)
             }
             
-            let datas = await fetchData(from: imageURLs)
-            saveImagesToDisk(datas: datas)
-            urls = loadImagesFromDisk()
+            for url in imageURLs {
+                do {
+                    guard let imageData = try await fetchData(from: url) else { return }
+                    
+                    let id = try ImageCache.shared.saveImageDataToDisk(imageData)
+                    ids.append(id)
+                    print(">>>", ids.count)
+                } catch {
+                    print(error)
+                }
+            }
         }
         
         try? await counselingSessionRepository.initialize()
@@ -69,16 +78,6 @@ extension TestEnvironmentManager {
         try? await counselingSessionRepository.save()
     }
     
-    private func loadImagesFromDisk() -> [URL] {
-        return ImageFileManager.shared.getAllImageURL()
-    }
-    
-    private func saveImagesToDisk(datas: [Data]) {
-        datas.forEach { data in
-            _ = try? ImageFileManager.shared.write(data: data, fileName: "image_\(UUID()).jpg")
-        }
-    }
-    
     private func fetchImageJSON(maxPage: Int) async -> [[String: Any]] {
         var datas: [[String: Any]] = []
         let baseURL = "https://picsum.photos/v2/list"
@@ -96,32 +95,23 @@ extension TestEnvironmentManager {
         return datas
     }
     
-    private func fetchData(from urls: [URL]) async -> [Data] {
-        var result: [Data] = []
-        
+    private func fetchData(from url: URL) async throws -> Data? {
         let configuration = URLSessionConfiguration.default
         configuration.timeoutIntervalForRequest = 10000
         configuration.timeoutIntervalForResource = 10000
         let session = URLSession(configuration: configuration)
         
-        await withTaskGroup(of: (Data?).self) { group in
-            for url in urls {
-                group.addTask {
-                    do {
-                        let (data, _) = try await session.data(from: url)
-                        return data
-                    } catch {
-                        print(error)
-                        return nil
-                    }
-                }
-            }
-            
-            for await data in group {
-                if let data = data { result.append(data) }
-            }
-        }
+        let (data, _) = try await session.data(from: url)
+        return data
+    }
+}
+
+private extension ImageCache {
+    @discardableResult
+    func saveImageDataToDisk(_ data: Data) throws -> UUID {
+        let id = UUID()
         
-        return result
+        guard let image = UIImage(data: data) else { throw LocalError(message: "invalid image") }
+        return try saveImageToDisk(image: image)
     }
 }
