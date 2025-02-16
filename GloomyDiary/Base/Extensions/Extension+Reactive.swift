032710bd58b3ea6cae1b2c2f13da6ec047488efc
LURKS02 <thecrooks2@gmail.com>
2025-02-17 08:49:53 +0900

    Combine cancellables 추가

--- GloomyDiary/Base/Extensions/Extension+Reactive.swift
+++ GloomyDiary/Base/Extensions/Extension+Reactive.swift
@@ -5,6 +5,9 @@
 //  Created by 디해 on 10/23/24.
 //
 
+import Combine
+import ObjectiveC
+
 import UIKit
 import RxSwift
 
@@ -22,6 +25,34 @@ extension Reactive where Base: AnyObject {
         }
         set {
             objc_setAssociatedObject(base, &disposeBagKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+}
+
+class CancellableHolder: NSObject {
+    var cb = Set<AnyCancellable>()
+}
+
+private var cancellablesKey: UInt8 = 0
+
+extension NSObject {
+    var cancellables: Set<AnyCancellable> {
+        get {
+            if let holder = objc_getAssociatedObject(self, &cancellablesKey) as? CancellableHolder {
+                return holder.cb
+            }
+            let holder = CancellableHolder()
+            objc_setAssociatedObject(self, &cancellablesKey, holder, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            return holder.cb
+        }
+        set {
+            if let holder = objc_getAssociatedObject(self, &cancellablesKey) as? CancellableHolder {
+                holder.cancellables = newValue
+            } else {
+                let holder = CancellableHolder()
+                holder.cb = newValue
+                objc_setAssociatedObject(self, &cancellablesKey, holder, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
         }
     }
 }
