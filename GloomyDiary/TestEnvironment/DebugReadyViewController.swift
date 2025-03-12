8658787cf2407d3619e8a01131af0adda74c5cdd
LURKS02 <thecrooks2@gmail.com>
2025-03-12 22:46:15 +0900

    파일 폴더 옮기기

--- GloomyDiary/TestEnvironment/DebugReadyViewController.swift
+++ GloomyDiary/TestEnvironment/DebugReadyViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 final class DebugReadyViewController: UIViewController {
+    
     private let label = UILabel().then {
         $0.text = "디버그 세팅 준비 중 ..."
         $0.textColor = .white
         $0.font = .systemFont(ofSize: 17)
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
     }
     
     override func viewDidLoad() {
@@ -21,6 +24,24 @@ final class DebugReadyViewController: UIViewController {
         view.addSubview(label)
         label.snp.makeConstraints { make in
             make.center.equalToSuperview()
+        }
+    }
+}
+
+extension DebugReadyViewController: TestEnvironmentManagerDelegate {
+    func didUpdateProcess(_ message: String) {
+        Task {
+            await MainActor.run {
+                label.text = message
+            }
+        }
+    }
+    
+    func didFinishPreparing() {
+        Task {
+            await MainActor.run {
+                self.dismiss(animated: false)
+            }
         }
     }
 }
