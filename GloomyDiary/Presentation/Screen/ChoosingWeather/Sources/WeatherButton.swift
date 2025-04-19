f048e2dce74e0db9aa8fd20bb89e387a49627c13
LURKS02 <thecrooks2@gmail.com>
2025-04-19 15:06:36 +0900

    이미지 및 JSON 추가

---
+++ GloomyDiary/Presentation/Screen/ChoosingWeather/Sources/WeatherButton.swift
@@ -0,0 +1,124 @@
+//
+//  WeatherCell.swift
+//  GloomyDiary
+//
+//  Created by 디해 on 10/28/24.
+//
+
+import UIKit
+
+final class WeatherButton: UIButton {
+
+    private enum Metric {
+        static let imageLeadingPadding: CGFloat = .deviceAdjustedWidth(20)
+        static let imageSize: CGFloat = .deviceAdjustedHeight(63)
+    }
+    
+    let identifier: String
+    
+    private let feedbackGenerator = UIImpactFeedbackGenerator(style: .soft)
+    
+    init(weather: Weather) {
+        self.identifier = weather.identifier
+        super.init(frame: .zero)
+        
+        setupConfiguration(with: weather)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.applyCircularShape()
+    }
+    
+    private func setupConfiguration(with weather: Weather) {
+        let image = AppImage.Component.weather(weather).image
+        self.setImage(image.resized(width: Metric.imageSize, height: Metric.imageSize), for: .normal)
+        
+        var configuration = UIButton.Configuration.plain()
+        
+        configuration.imagePlacement = .leading
+        configuration.imagePadding = Metric.imageLeadingPadding
+
+        var title = AttributedString(weather.name)
+        title.font = .온글잎_의연체.title
+        title.foregroundColor = AppColor.Text.main.color
+        configuration.attributedTitle = title
+        configuration.titleAlignment = .leading
+        configuration.background.backgroundColor = AppColor.Component.disabledSelectionButton.color
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 9, leading: 14, bottom: 9, trailing: 200)
+        
+        self.configuration = configuration
+    }
+}
+
+extension WeatherButton {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        var configuration = self.configuration
+        configuration?.background.backgroundColor = AppColor.Component.selectedSelectionButton.color.withAlphaComponent(0.3)
+        self.configuration = configuration
+        
+        feedbackGenerator.prepare()
+        feedbackGenerator.impactOccurred()
+        
+        AnimationGroup(
+            animations: [
+                Animation(
+                    view: self,
+                    animationCase: .transform( CGAffineTransform(scaleX: 0.95, y: 0.95)),
+                    duration: 0.1
+                )],
+            mode: .parallel,
+            loop: .once(completion: nil)
+        )
+        .run()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        
+        var contiguration = self.configuration
+        configuration?.background.backgroundColor = AppColor.Component.selectedSelectionButton.color.withAlphaComponent(0.3)
+        
+        self.configuration = configuration
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        
+        AnimationGroup(
+            animations: [
+                Animation(
+                    view: self,
+                    animationCase: .transform( .identity),
+                    duration: 0.1
+                )],
+            mode: .parallel,
+            loop: .once(completion: {} )
+        )
+        .run()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        
+        AnimationGroup(
+            animations: [
+                Animation(
+                    view: self,
+                    animationCase: .transform(
+                        .identity
+                    ),
+                    duration: 0.1
+                )],
+            mode: .parallel,
+            loop: .once(completion: {})
+        )
+        .run()
+    }
+}
